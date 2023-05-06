8c6deb31afc780031b47ac028b52d392ff2c3e3b
박준형 <revil.com@gmail.com>
2023-05-07 05:13:14 +0900

    feat: compression 적용

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,12 +1,15 @@
 import { ApiGatewayConfig } from '@app/core/configs';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as compression from 'compression';
 
 import { ApiGatewayModule } from './api-gateway.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
   const apiGatewayConfig = app.get(ConfigService).get<ApiGatewayConfig>('apiGateway');
+
+  app.use(compression());
 
   await app.listen(apiGatewayConfig.port, apiGatewayConfig.host);
 }
